960d2c8ab04836e1df4cd9fb9e5f2e90962de4f6
Elias Hernandis <elias@hernandis.me>
2024-07-18 11:57:37 +0200

    Add readthedocs build

--- docs/conf.py
+++ docs/conf.py
@@ -8,13 +8,13 @@ copyright = "2024, Elias Hernandis"
 author = "Elias Hernandis"
 
 # Read version from pyproject.toml
-with open("../../pyproject.toml", "r") as f:
+with open("../pyproject.toml", "r") as f:
     pyproject = toml.load(f)
     release = pyproject["tool"]["poetry"]["version"]
 
 # Make source available for autodoc
 os.environ["DJANGO_SETTINGS_MODULE"] = "tests.test_settings"
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath(".."))
 django.setup()
 
 
@@ -33,7 +33,6 @@ templates_path = ["_templates"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "alabaster"
-html_static_path = ["_static"]
 
 # -- Napoleon extension configuration ----------------------------------------
 
