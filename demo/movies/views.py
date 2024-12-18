7ed49f065aa4dd30c5eb033427f8caf6d6b291fa
Elias Hernandis <elias@hernandis.me>
2024-12-18 01:04:12 +0100

    Typo

--- demo/movies/views.py
+++ demo/movies/views.py
@@ -12,5 +12,4 @@ class MovieUpdateView(UpdateView):
     fields = ["title", "cover"]
 
     def get(self, request, *args, **kwargs):
-        # print(Movie._meta.get_field("cover"))
         return super().get(request, *args, **kwargs)
